0912f719983c672cc5468ac3aad30cc7c7a3aa9e
bilalseen <bilalsen2010@gmail.com>
2023-11-09 17:07:33 +0300

    Implemented a modification to remove empty inputs and NaN values between commas in the array. The array is now cleansed of invalid data and no longer contains unwanted values.

--- js-arrays-exercises/exercise-05/script.js
+++ js-arrays-exercises/exercise-05/script.js
@@ -4,75 +4,97 @@ document
     event.preventDefault();
 
     let userInput = document.getElementById("userInput").value;
-    let userInputArr = userInput.split(",");
-    userInputArr = userInputArr.map(function (numbers) {
-      return parseInt(numbers);
-    });
 
-    document.getElementById("userArrayOutput").innerHTML =
-      userInputArr.join(", ");
-
-    document
-      .getElementById("getPrimeNumbersButton")
-      .addEventListener("click", function () {
-        let primes = findPrimes(userInputArr);
-        console.log(primes.length);
-        if (primes.length > 0) {
-          document.getElementById("primeNumbersOutput").innerHTML =
-            primes.join(", ");
-        } else {
-          document.getElementById("primeNumbersOutput").innerHTML =
-            "No prime element.";
-        }
+    if (userInput == "") {
+      alert("Please enter your array");
+    } else {
+      console.log(userInput);
+      let userInputArr = userInput.split(",");
+      console.log(userInputArr);
+      userInputArr = userInputArr.filter(function (element) {
+        return element != 0;
       });
-
-    document
-      .getElementById("getEvenNumbersButton")
-      .addEventListener("click", function () {
-        let evens = findEvens(userInputArr);
-        document.getElementById("evenNumbersOutput").innerHTML =
-          evens.join(", ");
+      userInputArr = userInputArr.map(function (numbers) {
+        return parseInt(numbers);
       });
+      console.log(userInputArr);
 
-    document
-      .getElementById("getOddNumbersButton")
-      .addEventListener("click", function () {
-        let odds = findOdds(userInputArr);
-        document.getElementById("oddNumbersOutput").innerHTML = odds.join(", ");
-      });
+      document.getElementById("userArrayOutput").innerHTML =
+        userInputArr.join(", ");
 
-    function findPrimes(arr) {
-      let primeArr = [];
-      arr.forEach(function (number) {
-        let isPrime = true;
-        if (number < 0) lessThanZero = true;
-        for (let i = 2; i <= number / 2; i++) {
-          if (number % i === 0) {
-            isPrime = false;
-            break;
+      document
+        .getElementById("getPrimeNumbersButton")
+        .addEventListener("click", function () {
+          let primes = findPrimes(userInputArr);
+          console.log(primes.length);
+          if (primes.length > 0) {
+            document.getElementById("primeNumbersOutput").innerHTML =
+              primes.join(", ");
+          } else {
+            document.getElementById("primeNumbersOutput").innerHTML =
+              "there are no prime numbers";
           }
-        }
-        if (isPrime && number > 1) {
-          primeArr.push(number);
-        }
-      });
+        });
 
-      return primeArr;
-    }
+      document
+        .getElementById("getEvenNumbersButton")
+        .addEventListener("click", function () {
+          let evens = findEvens(userInputArr);
+          if (evens.length > 0) {
+            document.getElementById("evenNumbersOutput").innerHTML =
+              evens.join(", ");
+          } else {
+            document.getElementById("evenNumbersOutput").innerHTML =
+              "there are no even numbers";
+          }
+        });
 
-    function findEvens(arr) {
-      let evenNumbers = arr.filter(function (number) {
-        return number % 2 == 0;
-      });
+      document
+        .getElementById("getOddNumbersButton")
+        .addEventListener("click", function () {
+          let odds = findOdds(userInputArr);
+          if (odds.length > 0) {
+            document.getElementById("oddNumbersOutput").innerHTML =
+              odds.join(", ");
+          } else {
+            document.getElementById("oddNumbersOutput").innerHTML =
+              "there are no odd numbers";
+          }
+        });
 
-      return evenNumbers;
-    }
+      function findPrimes(arr) {
+        let primeArr = [];
+        arr.forEach(function (number) {
+          let isPrime = true;
+          if (number < 0) lessThanZero = true;
+          for (let i = 2; i <= number / 2; i++) {
+            if (number % i === 0) {
+              isPrime = false;
+              break;
+            }
+          }
+          if (isPrime && number > 1) {
+            primeArr.push(number);
+          }
+        });
 
-    function findOdds(arr) {
-      let oddNumbers = arr.filter(function (number) {
-        return number % 2 != 0;
-      });
+        return primeArr;
+      }
 
-      return oddNumbers;
+      function findEvens(arr) {
+        let evenNumbers = arr.filter(function (number) {
+          return number % 2 == 0;
+        });
+
+        return evenNumbers;
+      }
+
+      function findOdds(arr) {
+        let oddNumbers = arr.filter(function (number) {
+          return number % 2 != 0;
+        });
+
+        return oddNumbers;
+      }
     }
   });
